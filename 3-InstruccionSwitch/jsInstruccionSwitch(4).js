c58ce2491a2ea098cf0a8fea18053ab9c4645915
CuellarMaxi <36281554+CuellarMaxi@users.noreply.github.com>
2018-02-16 22:18:24 -0300

    1, 2 , 3, 4, 5 y 6

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -5,8 +5,33 @@ var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
 	
-	
+	switch(mesDelAño){
+		case "Enero":
+		case "Marzo":
+		case "Mayo":
+		case "Julio":
+		case "Agosto":
+		case "Diciembre"
+					alert("este mes tiene 31 dias.")
 
+		break
+
+		case "Febrero":
+					alert("este mes tiene 28 dias.")
+		break
+
+		case "Abril":
+		case "Noviembre":
+		case "Junio":
+		case "Septiembre":
+		case "Octubre":
+					alert("este mes tiene 30 dias.")
+		break
+	}
+
+//el que mas tiene podria tener default
+//								alert("este mes tiene X dias.")
+//
 
 
 }//FIN DE LA FUNCIÓN
